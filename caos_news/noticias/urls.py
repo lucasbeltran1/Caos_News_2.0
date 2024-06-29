a0b2b08669af33be8ac6087c56ba991b6a17b5f4
lucasbeltran1 <luca.beltran@duocuc.cl>
2024-06-29 11:47:16 -0400

    commit

--- caos_news/noticias/urls.py
+++ caos_news/noticias/urls.py
@@ -6,11 +6,22 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # Esta es la ruta para la URL raíz
     path('index', views.index, name='index'),  # Esto es redundante si solo deseas '/index'
+    path('apifutbol/', views.apifutbol, name='apifutbol'),
+    path('cine/', views.cine, name='cine'),
+    path('compra/', views.compra, name='compra'),
+    path('deporte/', views.deporte, name='deporte'),
+    path('espectaculo/', views.espectaculo, name='espectaculo'),
+    path('formulario/', views.formulario, name='formulario'),
+    path('lista_noticias/', views.lista_noticias, name='lista_noticias'),
+    path('musica/', views.musica, name='musica'),
+    path('noticia_editar/', views.noticia_editar, name='noticia_editar'),
+    path('publicar/', views.publicar, name='publicar'),
+    path('videojuegos/', views.videojuegos, name='videojuegos'),
     path('crud', views.crud, name='crud'),
     path('noticiasAdd', views.noticiasAdd, name='noticiasAdd'),
     path('noticias_del/<str:pk>', views.noticias_del, name='noticias_del'),
     path('noticias_findEdit/<str:pk>', views.noticias_findEdit, name='noticias_findEdit'),
-    
+    path('noticiasUpdate',views.noticiasUpdate, name='noticiasUpdate')
     # Otras rutas de la aplicación noticias
 ]
 
--- caos_news/noticias/views.py
+++ caos_news/noticias/views.py
@@ -5,16 +5,50 @@ def index(request):
     context = {}
     return render(request, 'noticias/index.html', context)
 
+def apifutbol(request):
+    return render(request, 'noticias/apifutbol.html')
+
+def cine(request):
+    return render(request, 'noticias/cine.html')
+
+def compra(request):
+    return render(request, 'noticias/compra.html')
+
+def deporte(request):
+    return render(request, 'noticias/deporte.html')
+
+def espectaculo(request):
+    return render(request, 'noticias/espectaculo.html')
+
+def formulario(request):
+    return render(request, 'noticias/formulario.html')
+
+def lista_noticias(request):
+    return render(request, 'noticias/lista_noticias.html')
+
+def musica(request):
+    return render(request, 'noticias/musica.html')
+
+def noticia_editar(request):
+    return render(request, 'noticias/noticia_editar.html')
+
+def publicar(request):
+    return render(request, 'noticias/publicar.html')
+
+def videojuegos(request):
+    return render(request, 'noticias/videojuegos.html')
+
+
 def crud(request):
     noticias = Noticia.objects.all()
     context = {'noticias': noticias}
     return render(request, 'noticias/lista_noticias.html', context)
 
 def noticiasAdd(request):
-    if request.method is not "POST":
+    if request.method != "POST":
         #debido a que no es un post, se muestra el formulario el formulario para agregar
         context = {}
-        return render(request, 'publicar.html', context)
+        return render(request, 'noticias/publicar.html', context)
     else:
 
 
@@ -32,7 +66,7 @@ def noticiasAdd(request):
                                     )
         obj.save()
         context={'mensaje' : "Datos guardados..."}
-        return render(request, 'publicar.html', context)
+        return render(request, 'noticias/publicar.html', context)
 
 def noticias_del(request,pk):
     context={}
@@ -43,9 +77,48 @@ def noticias_del(request,pk):
         mensaje="Datos eliminados..."
         noticias = Noticia.objects.all()
         context = {'noticias': noticias,    'mensaje': mensaje}
-        return render(request, 'lista_noticias.html', context)
+        return render(request, 'noticias/lista_noticias.html', context)
     except:
         mensaje="Error, el código no existe..."
         noticias = Noticia.objects.all()
         context = {'noticias': noticias,  'mensaje': mensaje}
-        return render(request, 'lista_noticias.html', context)
+        return render(request, 'noticias/lista_noticias.html', context)
+
+def noticias_findEdit(request,pk):
+
+    if pk != "":
+        noticia=Noticia.objects.get(codigo=pk)
+        
+        context={'noticia':noticia}
+        if noticia:
+            return render(request, 'noticia/noticia_editar.html', context)
+        else:
+            context={'mensaje':"Error, el código no existe..."}
+            return render(request, 'noticias/noticia_editar.html', context)
+        
+def noticiasUpdate(request):
+    if request.methos == "POST":
+        #es un POST, por lo tanto, se recuperan los datos del formulario y las graban en una tabla
+        codigo=request.POST["codigo"]
+        titulo=request.POST['titulo']
+        reportero=request.POST['reportero']
+        descripcion=request.POST['descripcion']
+        activo="1"
+
+        noticia= Noticia()
+        noticia.codigo=codigo
+        noticia.titulo=titulo
+        noticia.reportero=reportero
+        noticia.descripcion=descripcion
+        noticia.activo=1
+        noticia.save()
+
+        context={'mensaje':"Datos actualizados...", 'noticia':noticia}
+        return render(request, 'noticias/noticia_editar.html', context)
+    else:
+        #no es un post, nos mostraría el form de agregar
+        noticias = Noticia.objects.all()
+        context={'noticias':noticias}
+        return render(request, 'noticias/lista_noticias.html', context)
+
+
